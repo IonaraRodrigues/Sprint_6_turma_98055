d8b631ad1da0b0ed098d70cc436e1a73fd3fd1f9
IonaraRodrigues <ionara.sr@gmail.com>
2025-09-01 03:34:05 -0300

    Projeto corrigido e finalizado.

--- js/form.js
+++ js/form.js
@@ -2,12 +2,13 @@
 //class contato
 
 class contato {
-    constructor(nome, email,telefone, tipo, mensagem, aceitouTermos, aceitouNewsletter) {
+    constructor(nome, email, telefone, tipo, mensagem, aceitouTermos, aceitouNewsletter) {
         this.nome = nome;
         this.email = email;
         this.telefone = telefone;
         this.tipo = tipo;
         this.mensagem = mensagem;
+
         this.aceitouTermos = aceitouTermos;
         this.aceitouNewsletter = aceitouNewsletter;
     }
@@ -27,32 +28,32 @@ function Post(form) {
     const newsletterCheckbox = document.getElementById("newsletterCheckbox");
 
     if(!termosCheckbox.checked){
-        alert("Por favor, aceite os Termos e Condições para enviar o formulário.");
+        alert("Pro favor, aceite os Termos e Condições para enviar o formulário.");
         return false;
     }
 
-  let data = new contato(
-            form.elements.namedItem("nome").value, 
-            form.elements.namedItem("email").value, 
-            form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("tipo").value, 
-            form.elements.namedItem("mensagem").value,
-            termosCheckbox.checked,
-            newsletterCheckbox.checked
-    );       
-  
+    let data = new contato(
+        form.elements.namedItem("nome").value,
+        form.elements.namedItem("email").value,
+        form.elements.namedItem("telefone").value,
+        form.elements.namedItem("tipo").value,
+        form.elements.namedItem("mensagem").value,
+        termosCheckbox.checked,
+        newsletterCheckbox.checked
+    );
+
     console.log("Dados do formulário:", data);
 
-    alert(`Obrigado, ${data.nome}! Sua mensagem de ${data.tipo} foi enviado com sucesso.`);
+    alert(`Obrigado, ${data.nome}! Sua mensagem de ${data.tipo} foi enviada com sucesso.`);
 
     termosCheckbox.checked = false;
     newsletterCheckbox.checked = false;
 
     validarEnvio();
-    
+
     form.reset();
 }
 
-document.addEventListener('DOMContentLoaded', function(){ 
+document.addEventListener('DOMContentLoaded', function(){
     validarEnvio();
 })
